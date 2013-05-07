5a73bc697418dd6602644a4b945e648d0091f84a
Hareesh Ganesan <hareesh.ganesan@gmail.com>
2013-05-07 15:47:02 +0000

    login may be set at this point

--- Transcribe/urls.py
+++ Transcribe/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import *
 from Transcribe.views import hello, root
 
 # Uncomment the next two lines to enable the admin:
---
+++ TranscribeApp/static/js/base.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){});
+jQuery.toggleLogin = function()
+{
+	$("#auth").css("display", "none");
+	$("#login-form").fadeIn(function(){$("side-username").focus()});
+	return false;
+}
